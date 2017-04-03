6faf8c6bcd06200ebd0663fef474875df0377315
igor talic <igor talic>
2017-04-03 21:37:29 +0200

    Initial fix

---
+++ src/main/java/talic/movies/units/Movie.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package talic.movies.units;
+
+/**
+ *
+ * @author Talic
+ */
+public class Movie {
+
+   private String title;
+   private String link;
+
+   public Movie() {
+
+   }
+
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public void setLink(String link) {
+      this.link = link;
+   }
+
+   public String getTitle() {
+      return title;
+   }
+
+   public String getLink() {
+      return link;
+   }
+}
